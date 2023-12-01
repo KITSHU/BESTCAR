2c8487728fd674c8d820e31b5d7908620e173372
KITSHU <lucas2015kaka@hmail.com>
2023-12-01 11:30:42 -0300

    atualização

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let nome = 'lucas'
-let idade = 19
+let nome = 'lucas '
+let idade = 20
 let nome2 = 'carla'
-let idade2 = 18
+let idade2 = 19
+
+console.log(nome + nome2);
 console.log(nome,'tem',idade,'e tem uma namorada gostosa')
 console.log(nome2,'tem',idade2,'e é muito atraente pro',nome);
